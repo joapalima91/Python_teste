ef6a5c4ccc6bf88d28120e23ae1477bd95977642
João Pailol da Silva Lima <joaopalima91@gmail.com>
2022-06-10 16:58:57 -0300

    aula 10

---
+++ Condições em Python (if..else)/aula10.py
@@ -0,0 +1,24 @@
+
+# tempo = int(input('Quantos anos seu carro tem? '))
+# if tempo <=3:
+#     print('Carro novo!')
+# else:
+#     print("Carro velho")
+
+# nome = str(input('Qual é o seu nome? '))
+# if nome == 'João':
+#     print('Que nome lindo você tem!')
+# else:
+#     print('Seu nome é tão normal!')
+# print('Bom dia, {}!'.format(nome))
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2)/2
+print('A sua média foi {:.1f}'.format(m))
+
+print('Parabéns' if m >=6 else 'ESTUDE MAIS!') # if simplificado
+if m >= 6.0:
+    print('Sua média foi boa. Parabéns!!')
+else:
+    print('Sua média foi ruim. Estude mais!')
