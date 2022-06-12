da8169e24f25a700a0e2efcaf6f957cbcca05b7b
João Pailol da Silva Lima <joaopalima91@gmail.com>
2022-06-12 19:08:47 -0300

    ex33

--- Condições em Python (if..else)/ex0033_maior_menor.py
+++ Condições em Python (if..else)/ex0033_maior_menor.py
@@ -1,19 +1,20 @@
 #Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
-a = int(input('Digite primeiro número: ')
-b = int(input("Digite segundo número: "))
-c = int(input('Digite terceiro número: '))
-#verificando menor
-menor =a
+a = int(input('Digite primeiro valor: '))
+b = int(input('Digite segundo valor: '))
+c = int(input('Digite terceiro valor: '))
+#verificando menor valor
+menor = a
 if b<a and b<c:
     menor=b
 if c<a and c<b:
     menor=c
-#verificando maior
+
+#verfificando maior
 maior =a
 if b>a and b>c:
     maior=b
 if c>a and c>b:
     maior=c
-print("O menor valor digitado foi {}".format(menor))
-print("O maior valor digitado foi {}".format(maior))
+print('O menor valor é {}'.format(menor))
+print('O maior valor é {}'.format(maior))
